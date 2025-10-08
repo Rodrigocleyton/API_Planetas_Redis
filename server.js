f1ddbbc4dbef49a35d99eb8e47a851aa2d571e66
rodrigo <r.de.oliveira.porto@accenture.com>
2025-10-08 20:30:23 -0300

    Criação do arquivo server.js

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 require('dotenv').config()
 const express = require('express')
+const { getPlanetById } = require('./src/helper/axio-helper')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+  res.send({ message: 'Bem-vindo ao App de Planetas 🚀' })
+})
 
-app.listen(port, () =>{
-  console.log(`App rodando na porta ${port}`)
+app.get('/api/planets/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    const planet = await getPlanetById(id)
+    res.json(planet)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`🌍 App rodando na porta ${port}`)
 })
---
+++ src/helper/axio-helper.js
@@ -0,0 +1,17 @@
+// src/app/helper/axios-helper.js
+const axios = require('axios')
+
+async function getPlanetById(id) {
+  try {
+    const url = `https://swapi.dev/api/planets/${id}/`
+    const response = await axios.get(url)
+     console.log("response.data", response.data)
+    return response.data
+   
+  } catch (error) {
+    console.error(`❌ Erro ao buscar planeta: ${error.message}`)
+    throw new Error('Falha ao buscar dados do planeta')
+  }
+}
+
+module.exports = { getPlanetById }
