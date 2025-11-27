6aff5a63931ea88c1039597e892fd65176d1b0a7
Rodrigo Cleyton <r.de.oliveira.porto@accenture.com>
2025-11-27 18:06:44 -0300

    Ajuste no arquivo server

--- server.js
+++ server.js
@@ -19,25 +19,3 @@ app.listen(port, () =>{
   console.log(`App running in door ${port}`)
 })
 
-
-// require('dotenv').config()
-// const express = require('express')
-// const { requestLogger } = require('./src/middlewares/logger')          
-// const planetsRoutes = require('./src/routers/planets_routers')
-
-// const app = express()
-// const port = process.env.PORT || 3000
-
-// app.use(express.json())
-// app.use(requestLogger)
-
-// app.get('/', (req, res) => {
-//   res.send({ message: 'Bem-vindo ao App de Planetas ' })
-// })
-
-
-// app.use('/api/planets', planetsRoutes)
-
-// app.listen(port, () => {
-//   console.log(`App rodando na porta ${port}`)
-// })
