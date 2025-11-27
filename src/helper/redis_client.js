5623ebb513ef1fba5dc0dc3c84bc68913eba6c20
Rodrigo Cleyton <r.de.oliveira.porto@accenture.com>
2025-11-27 18:04:29 -0300

    Ajuste na funções do arquivo redis_client

--- src/helper/redis_client.js
+++ src/helper/redis_client.js
@@ -1,5 +1,4 @@
 const Redis = require('ioredis')
-//const { lazy } = require('react')
 
 const {
   REDIS_HOST,
@@ -12,73 +11,47 @@ const redis = new Redis({
   host: REDIS_HOST,
   port: Number(REDIS_PORT),
   db: Number(REDIS_DB),
-  namespace: NAMESPACE,
   maxRetriesPerRequest: 2,
   lazyConnect: true
 })
 
-const enduranceRedis = async () => {
+const ensureRedis = async () => {
    if(redis.status === 'end' || redis.status === 'wait') {
      await redis.connect()
    }
 }
 
-// const Redis = require('ioredis')
+const ns = (key) => {
+  return `${NAMESPACE}:${key}`
+}
 
-// const {
-//   REDIS_HOST,
-//   REDIS_PORT,
-//   REDIS_DB,
-//   NAMESPACE 
-// } = process.env
+const metricKey = (name) => {
+  return `${NAMESPACE}:metrics:${name}`
+}
 
-// const redis = new Redis({
-//   host: REDIS_HOST,
-//   port: Number(REDIS_PORT),
-//   db: Number(REDIS_DB),
-//   maxRetriesPerRequest: 2,
-//   lazyConnect: true
-// })
+const incrMetric = async (name) => {
+  await ensureRedis()
+  return redis.incr(metricKey(name))
+}
 
-// // Garante que o Redis está conectado
-// async function ensureRedis() {
-//   if (redis.status === 'end' || redis.status === 'wait') {
-//     await redis.connect()
-//   }
-// }
+const getMetric = async (name)=> {
+  await ensureRedis()
+  const value = await redis.get(metricKey(name))
+  return Number(value || 0)
+}
 
-// // Prefixo por ambiente → dev:planet:1
-// function ns(key) {
-//   return `${NAMESPACE}:${key}`
-// }
+const pingRedis = async () => {
+  await ensureRedis()
+  return redis.ping()
+}
 
-// // Métricas → dev:metrics:cache:hit
-// function metricKey(name) {
-//   return `${NAMESPACE}:metrics:${name}`
-// }
+module.exports = {
+  redis,
+  ensureRedis,
+  ns,
+  incrMetric,
+  getMetric,
+  pingRedis,
+ }
 
-// async function incrMetric(name) {
-//   await ensureRedis()
-//   return redis.incr(metricKey(name))
-// }
 
-// async function getMetric(name) {
-//   await ensureRedis()
-//   const value = await redis.get(metricKey(name))
-//   return Number(value || 0)
-// }
-
-// // health check (PONG)
-// async function pingRedis() {
-//   await ensureRedis()
-//   return redis.ping() // retorna 'PONG'
-// }
-
-// module.exports = {
-//   redis,
-//   ensureRedis,
-//   ns,
-//   incrMetric,
-//   getMetric,
-//   pingRedis
-// }
